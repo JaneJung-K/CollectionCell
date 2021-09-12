5aaeb8457d35fbeee6db7b676808fef329f81bea
JaneJung-K <piaojane@daum.net>
2021-09-12 14:19:32 +0900

    first commit

---
+++ CollectionCell/Table Cell/CollectionTableViewCell.swift
@@ -0,0 +1,54 @@
+//
+//  TableViewCell.swift
+//  CollectionCell
+//
+//  Created by mac on 2021/09/12.
+//
+
+import UIKit
+
+class CollectionTableViewCell: UITableViewCell, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+
+    
+    static let identifier = "CollectionTableViewCell"
+    
+    static func nib() -> UINib {
+        return UINib(nibName: "CollectionTableViewCell", bundle: nil)
+    }
+    
+    @IBOutlet var collectionView: UICollectionView!
+    
+    var models = [Model]()
+    
+    func configure(with models: [Model]) {
+        self.models = models
+        collectionView.reloadData()
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        collectionView.register(MyCollectionViewCell.nib(), forCellWithReuseIdentifier: MyCollectionViewCell.identifier)
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+    }
+
+    // Collectionview
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return models.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MyCollectionViewCell.identifier, for: indexPath) as! MyCollectionViewCell
+        cell.configure(with: models[indexPath.row])
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 250, height: 250)
+    }
+    
+}
+
